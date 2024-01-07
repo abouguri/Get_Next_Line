8e5fb42d0a3dc13693f6dfc06cc563f624d84d22
Abdelaadim Bougurine <abouguri@e1r12p3.1337.ma>
2024-01-07 16:41:56 +0100

    guess m done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abouguri <abouguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 21:53:13 by abouguri          #+#    #+#             */
-/*   Updated: 2024/01/04 22:10:58 by abouguri         ###   ########.fr       */
+/*   Updated: 2024/01/07 16:02:06 by abouguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(void)
 	int		fd;
 	char	*s;
 
-	fd = open("test.txt", O_RDONLY);
+	fd = open("test.txt", O_RDONLY | O_WRONLY);
 	s = get_next_line(fd);
 	while (s)
 	{
